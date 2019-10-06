aea51188596e6990eb9cba8e9878c3cbf4375efa
Eugenia Kim <elk303@nyu.edu>
2019-10-06 18:46:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    var array = []
+    for (var i=0; i < name.length; i++){
+        array.push("Thank you, " + name[i] + ", for the wonderful " + event + " gift!")
+    }
+    return array;
+}
+
+function countdown(number){
+    while (0<=number){
+        console.log(number)
+        number = (number - 1)
+    };
+};
